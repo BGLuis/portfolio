40724f94db9e258372cd27ced01481dc2bace316
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-08-29 08:26:41 -0300

    fix: deploy

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,6 +1,23 @@
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { starSystems } from './config/star-systems.config';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'galaxy/:body',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const params: { body: string }[] = [];
+      for (const system of starSystems) {
+        if (system.planets) {
+          for (const planet of system.planets) {
+            params.push({ body: planet.name });
+          }
+        }
+      }
+      return params;
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
